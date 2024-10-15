6d8f61d98c5525d75416b8fb136b2a31f73adf92
Matthew C. <mattdev23@protonmail.com>
2024-10-15 15:08:30 -0400

    prettier pass

--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -1,15 +1,15 @@
-import React from "react"
-import { createRoot } from 'react-dom/client'
-import App from "./App"
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
 
-const container = document.getElementById('app')
+const container = document.getElementById('app');
 if (container) {
-  const root = createRoot(container)
+  const root = createRoot(container);
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  )
+    </React.StrictMode>,
+  );
 } else {
-  console.error("Failed to find the root element")
+  console.error('Failed to find the root element');
 }
